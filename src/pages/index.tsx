8d9ab249fb99d970eb7a429b3387f27b8a25be43
Erick-Pavani <erickspavanii@gmail.com>
2022-06-07 20:34:40 -0300

    Finished 'Aula 3'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from 'next'
 import Title from '../ui/components/title/Title';
 import Lista from '../ui/components/Lista/Lista';
+import { Dialog, TextField, Grid, DialogActions, Button, Snackbar } from '@mui/material';
+import { useIndex } from '../data/hooks/pages/useIndex';
 
 const Home: NextPage = () => {
+  const {
+    listaPets,
+    petSelecionado,
+    setPetSelecionado,
+    email,
+    setEmail,
+    valor,
+    setValor,
+    mensagem,
+    setMensagem,
+    adotar
+  } = useIndex();
+
   return (
     <div>
       <Title
@@ -13,21 +28,56 @@ const Home: NextPage = () => {
             pode <strong>adotar um pet virtualmente</strong>
           </span>} />
       <Lista
-        pets={[
-          {
-            id: 1,
-            nome: "Lilica",
-            historia: 'Lilica foi encontrada no meio de uma tempestade de madrugada e na chuva, muito debilitada, foi cuidada pela instituição Luisa Mel e aguarda ansiosamente por uma nova família desde então.',
-            foto: 'https://www.correio24horas.com.br//fileadmin/_processed_/a/f/csm_doguinhodeterno_5a6fca5da3.jpg'
-          },
-          {
-            id: 2,
-            nome: "Rex",
-            historia: 'Filhotinho de Husky mui fófis cuti cuti',
-            foto: 'https://i0.wp.com/www.portaldodog.com.br/cachorros/wp-content/uploads/2021/04/pexels-photo-3726315.jpeg?resize=1000%2C667&ssl=1'
-          }
-        ]}
+        pets={listaPets}
+        onSelect={(pet) => setPetSelecionado(pet)}
       ></Lista>
+
+      <Dialog
+        open={petSelecionado !== null}
+        fullWidth
+        PaperProps={{ sx: { p: 5 } }}
+        onClose={() => setPetSelecionado(null)}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label={'E-mail'}
+              type={'email'}
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}>
+            </TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label={'Quantia por mês'}
+              type={'number'}
+              fullWidth
+              value={valor}
+              onChange={(e) => setValor(e.target.value)}>
+            </TextField>
+          </Grid>
+        </Grid>
+        <DialogActions sx={{ mt: 5 }}>
+          <Button
+            color={'secondary'}
+            onClick={() => setPetSelecionado(null)}
+          >
+            Cancelar
+          </Button>
+          <Button
+            variant={'contained'}
+            onClick={() => adotar()}>
+            Confirmar Adoção
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={mensagem.length > 0}
+        message={mensagem}
+        autoHideDuration={2500}
+        onClose={() => setMensagem('')}
+      />
     </div >
   )
 }
